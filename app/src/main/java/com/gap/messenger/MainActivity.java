ac05c65f8f5e7e0a2da1272b6b71398dbcf992e0
sergeygap <1ggserongg1@gmail.com>
2023-08-24 13:07:15 +0300

    add all designs for all activity

--- app/src/main/java/com/gap/messenger/MainActivity.java
+++ app/src/main/java/com/gap/messenger/MainActivity.java
@@ -1,14 +1,101 @@
 package com.gap.messenger;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivityBase";
+    private FirebaseAuth auth;
+
+    private TextView forgotPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+        clickListeners();
+        auth = FirebaseAuth.getInstance();
+
+        FirebaseUser user = auth.getCurrentUser();
+        if (user == null) {
+            Log.d(TAG, "user is not authorized");
+        } else {
+            Log.d(TAG, "Authorized " + user.getUid());
+        }
+        auth.signOut();
+        auth.signInWithEmailAndPassword("email@email.com", "1111111")
+                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        if (user == null) {
+                            Log.d(TAG, "user is not authorized");
+                        } else {
+                            Log.d(TAG, "Authorized " + user.getUid());
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(TAG, "onFailure: " + e.getMessage());
+                        Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+        auth.sendPasswordResetEmail("email@email.com");
+        auth.addAuthStateListener(new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                Log.d(TAG, "onAuthStateChanged: " + firebaseAuth);
+            }
+        });
+//        auth.createUserWithEmailAndPassword("email@email.com", "1111111")
+//                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+//                    @Override
+//                    public void onSuccess(AuthResult authResult) {
+//                        FirebaseUser user = auth.getCurrentUser();
+//                        if (user == null) {
+//                            Log.d(TAG, "user is not register");
+//                        } else {
+//                            Log.d(TAG, "Authorized");
+//                        }
+//                    }
+//                })
+//                .addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//                        Log.d(TAG, "onFailure: " + e.getMessage());
+//                        Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+//                    }
+//                });
+
+    }
+
+    private void clickListeners() {
+        forgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = ForgotPasswordActivity.newIntent(MainActivity.this);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initViews() {
+        forgotPassword = findViewById(R.id.forgot_password);
     }
 }
