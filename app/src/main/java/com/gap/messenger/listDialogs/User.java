da0f7372488a7c9affec8731330052727d535bde
sergeygap <1ggserongg1@gmail.com>
2023-08-31 11:35:10 +0300

    add data User

---
+++ app/src/main/java/com/gap/messenger/listDialogs/User.java
@@ -0,0 +1,38 @@
+package com.gap.messenger.listDialogs;
+
+public class User {
+
+    private String id;
+    private String name;
+    private String lastName;
+    private int age;
+    private Boolean isOnline;
+
+    public User(String id, String name, String lastName, int age, Boolean isOnline) {
+        this.id = id;
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
+        this.isOnline = isOnline;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Boolean getOnline() {
+        return isOnline;
+    }
+}
